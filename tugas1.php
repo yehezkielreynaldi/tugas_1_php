2041639d3d0cc5bc2b46148c3086a701a257478a
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2022-03-15 22:57:06 +0700

    Tugas 1 PHP
    Tugas ini saya kirimkan untuk team Niomic,Terimakasih.

---
+++ tugas1.php
@@ -0,0 +1,11 @@
+<?php
+$judul = "<h1>Daftar Menu</h1>";
+$isi = "<table><tr><td><li>Ayam Goreng</li></td><td>&nbsp; = Rp. 13000</td></tr><tr><td><li>Ayam Bakar</li></td><td>&nbsp; = Rp. 15000</td></tr><tr><td><li>Ayam Sayur</li></td><td>&nbsp; = Rp. 13000</td></tr><tr><td><li>Tempe Goreng</li></td><td>&nbsp; = Rp. 1000</td></tr>
+<tr><td><li>Tahu Goreng</li></td><td>&nbsp; = Rp. 1000</td></tr>
+<tr><td><li>Soto</li></td><td>&nbsp; = Rp. 3000</td></tr>
+<tr><td><li>Nasi Putih</li></td><td>&nbsp; = Rp. 5000</td></tr>
+<tr><td><li>Es Teh</li></td><td>&nbsp; = Rp. 4000</td></tr>
+<tr><td><li>Es Tebu</li></td><td>&nbsp; = Rp. 5000</td></tr></table>";
+
+echo "$judul";
+echo "$isi";
---
+++ tugas1_2.php
@@ -0,0 +1,11 @@
+<?php
+$judul = "<h1>Daftar Menu</h1>";
+$isi = "<table border='1'><tr><td>No</td><td>Nama Menu</td><td>Harga</td></tr><tr><td>1</td><td>Cireng</td><td>Rp 1000</td></tr><tr><td>2</td><td>Es Duren</td><td>Rp 15000</td></tr>
+<tr><td>3</td><td>Es Milo</td><td>Rp 20000</td></tr>
+<tr><td>4</td><td>Es Teh</td><td>Rp 5000</td></tr>
+<tr><td>5</td><td>Piscok</td><td>Rp 1000</td></tr></table>";
+
+
+
+echo "$judul";
+echo "$isi";
